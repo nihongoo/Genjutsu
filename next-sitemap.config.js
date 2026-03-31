2d97acc125218fc45d6cb47fbad11a3b0b284f98
Hachimi <haochuhuy@gmail.com>
2026-03-31 14:08:10 +0700

    add site map and update UI

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.genjutsu.id.vn/',
+  generateRobotsTxt: true, // Tự động tạo robots.txt
+  // (Tùy chọn khác: exclude, sitemapSize, ...)
+}
